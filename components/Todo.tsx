79e3c9e7344e299a45d4c810ac69eb124dae273a
Neha pandey <nehapandey@Nehas-MacBook-Pro.local>
2024-02-23 15:11:31 +0530

    todo.tsx

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 import pic from "../public/candra-winata-CYOFvtpOIpU-unsplash.jpg";
 export const Todo = () => {
   return (
     <div>
-        {/* <div className='flex min-h-screen items-center justify-center overflow-hidden bg-slate-100 '>
+      {/* <div className='flex min-h-screen items-center justify-center overflow-hidden bg-slate-100 '>
             <div className="group h-96 w-80 [perspective:1000px]">
                 <div className='relative h-full w-full rounded-xl shadow-xl transition-all duration-500[transform-style:preserve-3d]group-hover:[transform:rotateY(180deg)]'>
                     <div className="absolute inset-0">
@@ -23,7 +23,7 @@ export const Todo = () => {
             </div>
         </div> */}
 
-{/* <div className='flex min-h-screen items-center justify-center overflow-hidden bg-slate-100'>
+      {/* <div className='flex min-h-screen items-center justify-center overflow-hidden bg-slate-100'>
     <div className="group h-96 w-80 perspective-1000">
         <div className='relative h-full w-full rounded-xl shadow-xl transition-all duration-500 transform-style-preserve-3d group-hover:transform-rotate-y-180'>
             <div className="absolute inset-0">
@@ -41,7 +41,7 @@ export const Todo = () => {
     </div>
 </div> */}
 
-{/* <div className='pt-20 ml-10'>
+      {/* <div className='pt-20 ml-10'>
 <div className=" flip-card">
   <div className="flip-card-inner">
     <div className="flip-card-front">
@@ -56,22 +56,37 @@ export const Todo = () => {
 </div>
 
 </div> */}
-<div className="pt-20 ml-10">
-  <div className="flip-card relative w-64 h-64">
-    <div className="flip-card-inner absolute w-full h-full">
-      <div className="flip-card-front bg-gray-200">
-        <Image src={pic} alt="Avatar" className="w-full h-full object-cover rounded-xl" />
-      </div>
-      <div className="flip-card-back absolute w-full h-full bg-gray-800 text-white rounded-xl flex flex-col justify-center items-center">
-        <h1 className="text-2xl font-bold">John Doe</h1>
-        <p className="text-lg">Architect & Engineer</p>
-        <p>We love that guy</p>
+      <div className="pt-20 ml-10 rounded-xl">
+        <div className="flip-card relative  rounded-xl">
+          <div className="flip-card-inner absolute w-full h-full rounded-xl">
+            <div className="flip-card-front bg-gray-200 rounded-xl">
+              <Image
+                src={pic}
+                alt="Avatar"
+                className="w-full h-full object-cover rounded-xl"
+              />
+            </div>
+            <div className="flip-card-back  w-full h-full rounded-xl  pl-6  ">
+              <h1 className="title text-sm font-bold    pt-14 pl-6 ">
+                TITLE
+              </h1>
+              <p className="cycling text-md pl-6">cycling Task</p>
+              <div className="note-div rounded-lg pl-2 pt-2 mt-4 ml-4 mx-8  ">
+            <div className="note border rounded-3xl w-16  ">
+              <p className=" pl-2 ">Note </p>
+            </div>
+
+            <p className="text-and-tags mt-2 ml-0 font-thin  text-sm">
+              Lorem ipsum dolor sit. Lorem ipsum dolor,
+            </p>
+          </div>
+            </div>
+            
+          </div>
+
+          
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-
-    </div>
-  )
-}
+  );
+};
